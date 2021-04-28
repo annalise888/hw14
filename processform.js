9b87cc6df9bd05374be31c513334d6b3df8ea64c
annalise888 <52852720+annalise888@users.noreply.github.com>
2021-04-27 21:24:28 -0400

    Create processform.js

---
+++ processform.js
@@ -0,0 +1,59 @@
+var http = require('http');
+var fs = require('fs');
+var qs = require('querystring');
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
+const url = "mongodb+srv://annalisejacobson:oGNS0R1U2lycAjhR@cluster0.hhdio.mongodb.net/companydb?retryWrites=true&w=majority";
+
+http.createServer(function (req, res) {
+	if (req.url == "/") {
+		file = 'formpage.html';
+		fs.readFile(file, function(err, txt) {
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.write(txt);
+			res.end();
+		});
+	} else if (req.url == "/process") {
+		res.writeHead(200, {'Content-Type':'text/html'});
+		pdata = "";
+		req.on('data',data => {
+			pdata += data.toString();
+		});
+		req.on('end',()=> {
+			pdata = qs.parse(pdata);
+			
+			MongoClient.connect(url,{useUnifiedTopology:true},function(err, db) {
+				if (err) {
+					console.log("Connection err: " + err);
+				}
+				var dbo = db.db("companydb");
+				var coll = dbo.collection("companies");
+				var query;
+				if (pdata['radio'] == "name") {
+					query = {Company:pdata['txt']}
+				} else {
+					query = {Ticker:pdata['txt']}
+				}
+				coll.find(query).toArray(function(err, companies) {
+					if (err) {
+						console.log("Error: " + err);
+					} else if (companies.length == 0 ) {
+						console.log("Invalid company name");
+					} else {
+						console.log("Companies: ");
+						for (i=0; i<companies.length; i++) {
+							console.log("Company name: " + companies[i].Company + " Ticker: " + companies[i].Ticker);
+						}
+					}
+					db.close();
+				})
+				
+			})
+			res.end();
+		});
+	} else {
+		res.writeHead(200, {'Content-Type':'text/html'});
+		res.write("Unknown page request");
+		res.end();
+	}
+}).listen(8080);
